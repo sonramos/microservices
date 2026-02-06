a06291febfdff8739b589d83afd197cc86456b5c
Jackson Ramos <jackson.ramoosg@gmail.com>
2026-02-06 15:02:35 -0300

    microservices

---
+++ order/cmd/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+
+	"github.com/sonramos/microservices/order/config"
+	"github.com/sonramos/microservices/order/internal/adapters/db"
+	payment_adapter "github.com/sonramos/microservices/order/internal/adapters/payment"
+
+	"github.com/sonramos/microservices/order/internal/adapters/grpc"
+
+	"github.com/sonramos/microservices/order/internal/application/core/api"
+)
+
+func main() {
+
+	dbAdapter, err := db.NewAdapter(config.GetDataSourceURL())
+	if err != nil {
+		log.Fatalf("Failed to connect to database. Error: %v", err)
+	}
+
+	paymentAdapter, err := payment_adapter.NewAdapter(config.GetPaymentServiceUrl())
+	if err != nil {
+		log.Fatalf("Failed to initialize payment stub. Error: %v", err)
+	}
+
+	application := api.NewApplication(dbAdapter, paymentAdapter)
+	grpcAdapter := grpc.NewAdapter(application, config.GetApplicationPort())
+	grpcAdapter.Run()
+}
---
+++ order/internal/application/core/api/api.go
@@ -0,0 +1,30 @@
+package api
+
+import (
+	"github.com/sonramos/microservices/order/internal/application/core/domain"
+	"github.com/sonramos/microservices/order/internal/ports"
+)
+
+type Application struct {
+	db      ports.DBPort
+	payment ports.PaymentPort
+}
+
+func NewApplication(db ports.DBPort, payment ports.PaymentPort) *Application {
+	return &Application{
+		db:      db,
+		payment: payment,
+	}
+}
+
+func (a Application) PlaceOrder(order domain.Order) (domain.Order, error) {
+	err := a.db.Save(&order)
+	if err != nil {
+		return domain.Order{}, err
+	}
+	paymentErr := a.payment.Charge(&order)
+	if paymentErr != nil {
+		return domain.Order{}, paymentErr
+	}
+	return order, nil
+}
